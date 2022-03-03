3a65011ecdecba23a5756b5be2a3662c5837be64
Szilard Parrag <szilard.parrag@gmail.com>
2022-03-03 19:42:20 +0100

    lib: move example code to lib
    Signed-off-by: Szilard Parrag <szilard.parrag@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,7 @@
-use tower_lsp::jsonrpc::Result;
-use tower_lsp::lsp_types::*;
-use tower_lsp::{Client, LanguageServer, LspService, Server};
+use tower_lsp::{LspService, Server};
 
-#[derive(Debug)]
-struct Backend {
-    client: Client,
-}
+use lsp_syslog_ng::Backend;
 
-#[tower_lsp::async_trait]
-impl LanguageServer for Backend {
-    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        Ok(InitializeResult::default())
-    }
-
-    async fn initialized(&self, _: InitializedParams) {
-        self.client
-            .log_message(MessageType::INFO, "server initialized!")
-            .await;
-    }
-
-    async fn shutdown(&self) -> Result<()> {
-        Ok(())
-    }
-}
 
 #[tokio::main]
 async fn main() {
